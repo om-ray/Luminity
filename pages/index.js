6ba68a5134755358f68bb0584f266000a3aa5d6a
123om123 <omihridesh@gmail.com>
2020-07-10 19:01:28 -0400

    css

--- pages/index.js
+++ pages/index.js
@@ -11,32 +11,46 @@ let useStyles = makeStyles(() => ({
   root: {
     // flexGrow: 1,
   },
+  megrim: {
+    fontFamily: "Megrim",
+  },
+  avenir: {
+    fontFamily: "Avenir",
+  },
   navBar: {
     display: "flex",
     position: "absolute",
     top: 0,
     width: "100vw",
-    justifyContent: "space-around",
+    justifyContent: " space-around",
     alignItems: "center",
     position: "fixed",
-    padding: "20px",
-    background: "rgb(73, 79, 82)",
-    color: "white",
+    padding: "17px",
+    borderBottom: "#2c2c2c solid 1px !important",
+    background: "#141415",
+    color: "#909090",
     marginBottom: "3vh",
   },
   title: {
-    fontFamily: "avenir",
+    fontFamily: "Avenir",
   },
   logo: {
+    background: "#232526",
     fontFamily: "Megrim",
+    borderRadius: "100px",
+    padding: "10px 30px",
+    color: "white",
+    fontWeight: 800,
+    fontSize: "30px",
   },
   navLinks: {
-    fontFamily: "avenir",
+    fontFamily: "Megrim",
     width: "40%",
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
     listStyle: "none",
+    color: "white",
   },
 }));
 
@@ -51,8 +65,12 @@ export default function ButtonAppBar() {
             Luminity
           </Typography>
           <div className={classes.navLinks}>
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Login</Button>
+            <Button className={classes.avenir} color="inherit">
+              About
+            </Button>
+            <Button className={classes.avenir} color="inherit">
+              Login
+            </Button>
           </div>
         </Toolbar>
       </AppBar>
